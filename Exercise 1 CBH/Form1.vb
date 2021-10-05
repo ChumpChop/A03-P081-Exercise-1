84af4a49a4a6417378850cd0075ae0c1513d1883
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-05 22:41:40 +0100

    Add project files.

---
+++ Exercise 1 CBH/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intOne As Integer = 1
+        Dim intTwo As Integer = 2
+
+        MessageBox.Show((intOne + intTwo).ToString, "1 + 2")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strA As String = "A"
+        Dim strB As String = "B"
+
+        MessageBox.Show(strA & strB, "A concatenated with B")
+    End Sub
+End Class
